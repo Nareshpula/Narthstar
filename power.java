17b35bdbec0cd4641cb9eeaed57b2abfc908d182
NareshRoyal <NareshRoyal>
2017-06-06 23:03:27 -0700

    i can delete the option by the power.java

--- power.java
+++
@@ -1,4 +0,0 @@
-this is the power to maintain the gap to their brother.
-he is also very good persion and he took the good decisions about the people.
-they can esy to grap the power to maintain in their house.
-this is the best thing about the their personality.
